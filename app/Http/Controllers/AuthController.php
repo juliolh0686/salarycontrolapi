1a000f58767548814f7773380b7f95e4a15d19a6
Julio Loarte <juliolh06@gmail.com>
2023-02-12 19:52:15 -0500

    Add AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+
+        try{
+
+            //Validated
+            $validator = Validator::make($request->all(),
+            [
+                'name' => 'required|string|max:255',
+                'email' => 'required|email|max:255|unique:users,email',
+                'password' => 'required|string|min:8'
+            ]);
+
+            if($validator->fails()){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $validator->errors()
+                ], 401);
+            }
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User Created Successfully',
+                'token' => $user->createToken("API TOKEN")->plainTextToken
+            ], 200);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+        
+    }
+
+    public function login(Request $request) {
+
+      try {
+
+        $validator = Validator::make($request->all(),
+        [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+          if($validator->fails()){
+              return response()->json([
+                  'status' => false,
+                  'message' => 'validation error',
+                  'errors' => $validator->errors()
+              ], 401);
+          }
+
+          if(!Auth::attempt($request->only(['email', 'password']))){
+              return response()->json([
+                  'status' => false,
+                  'message' => 'Email & Password does not match with our record.',
+              ], 401);
+          }
+
+          $user = User::where('email', $request->email)->first();
+
+          return response()->json([
+              'status' => true,
+              'message' => 'User Logged In Successfully',
+              'token' => $user->createToken("API TOKEN")->plainTextToken
+          ], 200);
+
+      } catch (\Throwable $th) {
+          return response()->json([
+              'status' => false,
+              'message' => $th->getMessage()
+          ], 500);
+      }
+    }
+
+    public function logout() {
+
+      auth()->user()->tokens()->delete();
+
+      return [
+        'message'=> 'Logout Satisfactorio'
+      ];
+    }
+}
